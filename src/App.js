1dc26c8fca6996dbe9ef4a108b9d079c2a708d66
pray-lee <praycis@gmail.com>
2019-12-16 18:14:48 +0800

    update react-demo

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import React from 'react';
+console.log(React)
+
+// tudo list
+class TodoApp extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: [],
+      text: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+  render() {
+    return (
+      <div>
+        <h3>TODO</h3>
+        <TodoList items={this.state.items}></TodoList>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="new-todo">
+            What needs to be done?<br />
+          </label>
+          <input id="new-todo" onChange={this.handleChange} value={this.state.text} />
+          <button>Add #{this.state.items.length + 1}</button>
+        </form>
+      </div>
+    )
+  }
+
+  handleChange(e) {
+    this.setState({ text: e.target.value })
+  }
+
+  handleSubmit(e) {
+    e.preventDefault()
+    if (!this.state.text.length) {
+      return
+    }
+    const newItem = {
+      text: this.state.text,
+      id: Date.now()
+    }
+    this.setState(state => ({
+      items: state.items.concat(newItem),
+      text: ''
+    }))
+  }
+}
+
+const TodoList = (props) => {
+  return (
+    <ul>
+      {props.items.map(item => (
+        <li key={item.id}>{item.text}</li>
+      ))}
+    </ul>
+  )
+}
+
+export default TodoApp;
