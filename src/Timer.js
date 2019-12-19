4a48611824d83bd5e4cb2b298a9ca4f09286c729
pray-lee <praycis@gmail.com>
2019-12-19 14:06:20 +0800

    react fragment

--- src/Timer.js
+++ src/Timer.js
@@ -1,27 +1,42 @@
 import React from 'react'
+
 export default class Timer extends React.Component {
-   constructor(props) {
-       super(props)
-       this.state = {
-           seconds: 0
-       }
-   } 
-   tick () {
-       this.setState(state => ({
-           seconds: state.seconds + 1
-       }))
-   }
-   componentDidMount() {
-       this.interval = setInterval(() => this.tick(), 1000)
-   }
+  constructor(props) {
+    super(props)
+    this.state = {
+      seconds: 0
+    }
+  }
 
-   componentWillUnmount() {
-       clearInterval(this.interval)
-   }
+  tick() {
+    this.setState(state => ({
+      seconds: state.seconds + 1
+    }))
+  }
 
-   render() {
-       return (
-           <div>timer components: <p>Seconds: {this.state.seconds}</p></div>
-       )
-   }
+  componentDidMount() {
+    this.interval = setInterval(() => this.tick(), 1000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval)
+  }
+
+  render() {
+    // aria-label 无障碍辅助功能
+    const labelText = 'Web Accessibility Initiative - Accessible Rich Internet Applications'
+    return (
+      <div aria-label={labelText}>
+        timer components:
+        <p>Seconds: {this.state.seconds}</p>
+        {/*same as vue template tag*/}
+        <React.Fragment>
+          <i></i>
+          <i></i>
+          <i></i>
+          <i></i>
+        </React.Fragment>
+      </div>
+    )
+  }
 }
